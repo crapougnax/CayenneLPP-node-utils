09acb71d64d572d9d9f9c0ced302ec194b62ba50
Olivier Lépine <olivier@lepine.fr>
2018-11-07 17:58:54 +0100

    ES6 Conversion and decoder addition

---
+++ test/decoder.js
@@ -0,0 +1,42 @@
+require('babel-register')
+
+import decoder from '../lib/decoder'
+import { assert } from 'chai'
+
+function getDecoder() {
+    const buffer = Buffer.from('016700E9026838036701040468530402014901020126', 'hex')
+    const cyn = new decoder(buffer)
+    cyn.decode()
+
+    return cyn
+}
+
+describe('decoder', () => {
+
+    const cyn = getDecoder()
+    const channel1 = getDecoder().getChannel(1)
+    const channel3 = getDecoder().getChannel(3)
+
+    console.log(cyn)
+
+    it('data should contain 4 channels', function() {
+        assert.equal(4, Object.keys(cyn.getChannels()).length)
+    })
+
+    it('channel 1 & 3 should exist', function() {
+        assert.typeOf(channel1, 'object')
+        assert.typeOf(channel3, 'object')
+    })
+
+    it('channel 5 should not exist', function() {
+        assert.isFalse(getDecoder().getChannel(5))
+    })
+
+    it('channel 1 should contain temperature and analog properties', function() {
+        assert.hasAllKeys(channel1, ['temperature', 'analog'])
+    })
+
+    it('temperature on channel 1 should be 23.3', function() {
+        assert.strictEqual(23.3, channel1.temperature)
+    })
+})
--- test/encoder.js
+++ test/encoder.js
@@ -1,20 +1,22 @@
-const LPPEncoder = require('../lib').LPPEncoder;
-const assert = require('assert');
+require('babel-register')
+
+import encoder from '../lib/encoder'
+import assert from "assert"
 
 describe('encoder', () => {
     it('should return a valid temperature payload', () => {
-        const cyn = new LPPEncoder();
-        cyn.addTemperature(99, 33.0);
-        const pl = cyn.getPayload().toString('base64');
-        assert.equal('Y2cBSg==', pl);
-    });
+        const cyn = new encoder()
+        cyn.addTemperature(99, 33.0)
+        const pl = cyn.getPayload().toString('base64')
+        assert.equal('Y2cBSg==', pl)
+    })
 
     it('should return a valid payload with multiple channels', () => {
-        const cyn = new LPPEncoder();
-        cyn.addAnalogInput(13, -13.131313);
-        cyn.addTemperature(99, 33.2);
-        cyn.addDigitalInput(10, 1);
-        const pl = cyn.getPayload().toString('base64');
-        assert.equal('DQL63mNnAUwKAAE=', pl);
-    });
-});
+        const cyn = new encoder()
+        cyn.addAnalogInput(13, -13.131313)
+        cyn.addTemperature(99, 33.2)
+        cyn.addDigitalInput(10, 1)
+        const pl = cyn.getPayload().toString('base64')
+        assert.equal('DQL63mNnAUwKAAE=', pl)
+    })
+})
